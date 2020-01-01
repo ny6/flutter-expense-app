73bfcbdb4e659429675f54ccc266c8f824d17619
Yeshu Kochher <yeshu@outlook.in>
2020-01-01 23:03:01 +0530

    one where more responsiveness

--- lib/main.dart
+++ lib/main.dart
@@ -90,12 +90,44 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final isLandscape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
+
     final appBar = AppBar(
       title: Text('Expense App'),
       actions: <Widget>[
         IconButton(
           icon: Icon(Icons.add),
           onPressed: () => _startAddNewTransaction(context),
+        ),
+      ],
+    );
+
+    final viewHeight = MediaQuery.of(context).size.height -
+        appBar.preferredSize.height -
+        MediaQuery.of(context).padding.top;
+
+    final txListWidget = Container(
+      height: viewHeight * 0.7,
+      child: TransactionList(_userTransactions, _deleteTransaction),
+    );
+
+    Widget getChartWidget(double height) => Container(
+          height: viewHeight * height,
+          child: Chart(_recentTransactions),
+        );
+
+    final showChartWidget = Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text('Show Chart'),
+        Switch(
+          value: _showChart,
+          onChanged: (val) {
+            setState(() {
+              _showChart = val;
+            });
+          },
         ),
       ],
     );
@@ -110,36 +142,10 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text('Show Chart'),
-                  Switch(
-                    value: _showChart,
-                    onChanged: (val) {
-                      setState(() {
-                        _showChart = val;
-                      });
-                    },
-                  ),
-                ],
-              ),
-              _showChart
-                  ? Container(
-                      height: (MediaQuery.of(context).size.height -
-                              appBar.preferredSize.height -
-                              MediaQuery.of(context).padding.top) *
-                          0.7,
-                      child: Chart(_recentTransactions),
-                    )
-                  : Container(
-                      height: (MediaQuery.of(context).size.height -
-                              appBar.preferredSize.height -
-                              MediaQuery.of(context).padding.top) *
-                          0.7,
-                      child: TransactionList(
-                          _userTransactions, _deleteTransaction),
-                    ),
+              if (!isLandscape) getChartWidget(0.3),
+              if (!isLandscape) txListWidget,
+              if (isLandscape) showChartWidget,
+              if (isLandscape) _showChart ? getChartWidget(0.7) : txListWidget,
             ],
           ),
         ),
