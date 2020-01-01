9806fc9a721feb7abe7ffe06a1fdf8e87dbf2f24
Yeshu Kochher <yeshu@outlook.in>
2020-01-01 16:30:28 +0530

    one where more improvements

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -12,7 +12,10 @@ class ChartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        FittedBox(child: Text('\$${amount.toStringAsFixed(2)}')),
+        Container(
+          height: 20,
+          child: FittedBox(child: Text('\$${amount.toStringAsFixed(2)}')),
+        ),
         SizedBox(height: 4),
         Container(
           height: 60,
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -30,39 +30,25 @@ class TransactionList extends StatelessWidget {
             )
           : ListView.builder(
               itemBuilder: (ctx, index) => Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: getPrimaryColor(context),
-                          width: 2,
-                        ),
-                      ),
-                      child: Text(
-                        '\$${transactions[index].amount.toStringAsFixed(2)}',
-                        style: TextStyle(
-                          color: getPrimaryColor(context),
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
+                margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                elevation: 5,
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 30,
+                    child: Padding(
+                      padding: EdgeInsets.all(6.0),
+                      child: FittedBox(
+                        child: Text(
+                            '\$${transactions[index].amount.toStringAsFixed(2)}'),
                       ),
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          transactions[index].title,
-                          style: Theme.of(context).textTheme.title,
-                        ),
-                        Text(DateFormat.yMMMd()
-                            .format(transactions[index].date)),
-                      ],
-                    )
-                  ],
+                  ),
+                  title: Text(
+                    transactions[index].title,
+                    style: Theme.of(context).textTheme.title,
+                  ),
+                  subtitle:
+                      Text(DateFormat.yMMMd().format(transactions[index].date)),
                 ),
               ),
               itemCount: transactions.length,
